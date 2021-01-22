5c0a1c268668ba947a41f306150de109bf49e129
yangpeng-coder <yp.asolu@gmail.com>
2021-01-22 17:58:49 +0900

    简单登录页面和主页

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,10 +1,47 @@
 <template>
-
+    <div>
+        <label>
+            user:<input type="text" v-model="loginForm.username">
+        </label>
+        <br><br>
+        <label>
+            password:<input type="password" v-model="loginForm.password">
+        </label>
+        <br><br>
+        <button type="button" @click="login">login</button>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'Login.vue'
+    name: 'Login.vue',
+    data () {
+        return {
+            loginForm: {
+                username: '',
+                password: ''
+            },
+            responseResult: []
+        }
+    },
+    methods: {
+        login () {
+            this.$axios
+                .post('/login', {
+                    username: this.loginForm.username,
+                    password: this.loginForm.password
+                })
+                .then(successResponse => {
+                    if (successResponse.data.code === 200) {
+                        this.$router.replace({path: '/index'})
+                    } else if (successResponse.data.code === 400) {
+                        alert('failed.')
+                    }
+                })
+                .catch(failResponse => {
+                })
+        }
+    }
 }
 </script>
 
